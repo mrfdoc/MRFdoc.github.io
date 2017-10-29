588922641b8c122b0afc289be4dda76ee1cbbba6
muhammad Rizal <officialrizalfahmi@gmail.com>
2017-10-29 15:32:07 +0700

    image responsive

--- index.js
+++ index.js
@@ -1,3 +1,67 @@
-function toggleSidebar (){
-    document.getElementById("sidebar").classList.toggle('active');
+Number
+const n = 10;
+
+String
+const apos_string = 'Hello';
+const quot_string = "Hello";
+const tick_string = `Hello`;
+
+console.log(apos_string === quot_string)
+
+const kalimat = `${apos_string} ${n}`;
+kalimat
+
+const enter = `a
+
+b`
+enter
+
+// Boolean
+const b = true
+
+y = apos_string + b
+y
+
+x = Boolean('b')
+x
+
+// Array
+r = [n, n, quot_string, n, n, n];
+r
+
+
+rs = Array.from(apos_string);
+
+console.log(apos_string.length)
+
+console.log(r[0])
+
+// Object = sesuatu yang kompleks
+const o = {
+    Nama: 'Rizal',
+    Kelas: 'Web',
+    Arr: r,
+    Partner: {
+        Nama: 'Rio',
+        Kelas: 'Web',
+    namadankelas: function(){
+        return this.Nama + this.Kelas
+    }
+    }
 }
+console.log(o.Arr);
+console.log(o.Nama);
+console.log(o.Partner.Nama);
+
+// Funcion
+function tambah(a, b) {
+    return a + b
+}
+
+console.log(tambah(100, 105522))
+
+function kali(a, b) {
+    return a * b
+};
+
+console.log(kali(100,233))
