da03a5cc9b0d6f27c70a72daff60d7b43b2dd527
muhammad Rizal <officialrizalfahmi@gmail.com>
2017-11-02 23:07:09 +0700

    New Content

--- js/function.js
+++ js/function.js
@@ -1,8 +1,13 @@
-$(window).scroll(function() {
-   if ($(document).scrollTop() > 50) {
-       $('nav').addClass('header');
-   }
-   else {
-       $('nav').removeClass('header');
-   }
-});
+$(window).scroll(function () {
+    var wScroll = $(this).scrollTop();
+
+    $('.latar').css({
+        'transform': 'translate(0px, -' + wScroll / 20 + '%)'
+    });
+    $('.latar h1').css({
+        'transform': 'translate(0px, -' + wScroll / 40 + '%)'
+    });
+    $('.latar h2').css({
+        'transform': 'translate(0px, -' + wScroll + '%)'
+    });
+    });
