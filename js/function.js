f95bdd8c38252ef6505f4b6ea98d01ecc8a452f2
muhammad Rizal <officialrizalfahmi@gmail.com>
2017-11-04 09:34:46 +0700

    parallax index

--- js/function.js
+++ js/function.js
@@ -4,10 +4,17 @@ $(window).scroll(function () {
     $('.latar').css({
         'transform': 'translate(0px, -' + wScroll / 20 + '%)'
     });
+    
     $('.latar h1').css({
         'transform': 'translate(0px, -' + wScroll / 20 + '%)'
     });
+    
     $('.latar h2').css({
         'transform': 'translate(0px, -' + wScroll + '%)'
     });
+
+    $('.back').css({
+        'transform': 'translate(0px, -' + wScroll / 80 + '%)'
     });
+
+});
